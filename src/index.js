a0069fc09774d5ac74ad879673d8b57937a18e64
Guillermo Morris <gmorris.training@wowdesarrollos.com>
2023-03-03 11:33:48 -1100

    autentificacion en express

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-const guardar = require("./Routes/list-edit-router");
-const incompletas = require("./Routes/list-view-router");
-const completadas = require("./Routes/list-view-router");
-const eliminar = require("./Routes/list-delete-router");
-const actualizar = require("./Routes/list-update-router");
 const express = require("express");
-const { ValidacionPost, validacionPut, requestValid, validUrl } = require("./Middleware/middleware");
+const router = require("./Routes/routes");
 const app = express();
 const port = 3001;
+const host = "localhost";
+const {
+  validUrl,
+  requestValid,
+  validacionPost,
+} = require("./Middleware/middleware");
+const login = require("./login");
+
+//set
+app.set("view engine", "ejs");
 
 //middleware
+
+// app.use("/recursos", express.static(__dirname + "/public"));
 app.use(express.json());
-app.use(ValidacionPost);
-app.use(validacionPut);
-app.use(requestValid)
-app.use(validUrl)
-
-app.use("/", incompletas);
-app.use("/", completadas);
-app.use("/", guardar);
-app.use("/", eliminar);
-app.use("/", actualizar);
-
-app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto: http://localhost:${port}`);
+app.use(requestValid);
+app.use(validUrl);
+app.use(validacionPost);
+//Route
+app.use("/app", login);
+app.use("/app", router);
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-module.exports = app;
+//Server
+app.listen(3001, () => console.log(` servidor en http://${host}:${port}`));
